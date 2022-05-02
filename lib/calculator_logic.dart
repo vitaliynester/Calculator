c161be1062d521c9237cef55b9441efdd7913d55
Vitaly Posadnev <elemabor@gmail.com>
2022-05-02 21:56:24 +0300

    feat(logic): remove dot char from end

--- lib/calculator_logic.dart
+++ lib/calculator_logic.dart
@@ -27,7 +27,22 @@ class Calculator {
 
   String removeLast(String exp) {
     if (exp.length > 1) {
-      return exp.substring(0, exp.length - 1);
+      var value = exp.substring(0, exp.length - 1);
+      var char = value[value.length - 1];
+      if (value.length > 1 &&
+          (char != '0' &&
+              char != '1' &&
+              char != '2' &&
+              char != '3' &&
+              char != '4' &&
+              char != '5' &&
+              char != '6' &&
+              char != '7' &&
+              char != '8' &&
+              char != '9')) {
+        return removeLast(value);
+      }
+      return value;
     } else {
       return "0";
     }
