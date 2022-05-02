aa18b99686438ca3d83b05fc4652b2a0bf1ee1d8
Vitaly Posadnev <elemabor@gmail.com>
2022-05-02 21:40:32 +0300

    feat(providers): create calculator provider

---
+++ lib/calculator_provider.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import 'calculator_logic.dart';
+
+class CalculatorProvider extends ChangeNotifier {
+  String _resultCalcultation;
+  String _helpedText;
+  Calculator _logic;
+
+  String get resultCalculation => _resultCalcultation;
+  String get helpedText => _helpedText;
+
+  CalculatorProvider() {
+    _logic = Calculator();
+    _resultCalcultation = "0";
+    _helpedText = "";
+  }
+
+  void addNumber(String number) {
+    _resultCalcultation =
+        _resultCalcultation == "0.0" || _resultCalcultation == "0"
+            ? number
+            : _resultCalcultation + number;
+    notifyListeners();
+  }
+
+  void addOperation(String operation) {
+    if (operation == ".") {
+      if (_resultCalcultation.contains(".")) {
+        return;
+      } else {
+        _resultCalcultation += operation;
+      }
+    } else {
+      if (_resultCalcultation[_resultCalcultation.length - 1] != "÷" &&
+          _resultCalcultation[_resultCalcultation.length - 1] != "x" &&
+          _resultCalcultation[_resultCalcultation.length - 1] != "-" &&
+          _resultCalcultation[_resultCalcultation.length - 1] != "+") {
+        _helpedText =
+            _logic.calc(_helpedText + _resultCalcultation) + operation;
+        _resultCalcultation = "0.0";
+      }
+    }
+    notifyListeners();
+  }
+
+  void funcAC() {
+    _resultCalcultation = _logic.ac();
+    notifyListeners();
+  }
+
+  void funcPlusMinus() {
+    _resultCalcultation = _logic.plusMinus(_resultCalcultation);
+    notifyListeners();
+  }
+
+  void funcRemoveLast() {
+    _resultCalcultation = _logic.removeLast(_resultCalcultation);
+    notifyListeners();
+  }
+
+  void funcEquals() {
+    _resultCalcultation = _logic.calc(_helpedText + _resultCalcultation);
+    _helpedText = "";
+    notifyListeners();
+  }
+}
