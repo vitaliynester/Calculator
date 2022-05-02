30283ef40ce98172a0e6d45caca82e0b8ff2c0f2
Vitaly Posadnev <elemabor@gmail.com>
2022-05-02 21:40:54 +0300

    feat(mail): add provider to main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'calculator_provider.dart';
 import 'calculator_view.dart';
 
 void main() => runApp(MyApp());
@@ -7,10 +9,15 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: CalculatorView(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CalculatorProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: CalculatorView(),
+        ),
       ),
     );
   }
